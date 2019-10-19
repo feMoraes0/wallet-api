0df61a29ba9f20e22af90b13e234bbaf4aa5e5f7
feMoraes0 <fernandomoraes.lopes@gmail.com>
2019-10-19 20:52:42 -0300

    Adding partial show and store card functions

--- app/controllers/CardController.js
+++ app/controllers/CardController.js
@@ -1,4 +1,5 @@
 const Card = require('../models/Card');
+const User = require('../models/User');
 
 module.exports = {
   
@@ -7,11 +8,35 @@ module.exports = {
   },
 
   async show(request, response, next) {
-    return response.status(200).send({'msg': 'success'});
+    const {user_id, card_id} = request.params;
+
+    const card = await Card.findOne({
+      where: {
+        id: parseInt(card_id),
+        user_id: parseInt(user_id)
+      }
+    });
+
+    return response.status(200).send(card);
   },
   
   async store(request, response, next) {
-    return response.status(200).send({'msg': 'success'});
+    const { user_id, color_id, label, coin, type, initial } = request.body;
+
+    if(!user_id || !color_id || !label|| !coin || !type)
+      return response.status(400).send({'msg': 'Some required fields are not sent.'});
+    
+    if(user_id == '' || color_id == '' || label == '' || coin == '' || type == '')
+      return response.status(400).send({'msg': 'Fields can not be empty.'});
+    
+    const user = await User.findByPk(parseInt(user_id));
+
+    if(!user)
+      return response.status(404).send({'msg': 'User can not be found.'});
+
+    const card = await Card.create({user_id, color_id, label, coin, type,});
+
+    return response.status(200).send({'card': card});
   },
   
   async update(request, response, next) {
