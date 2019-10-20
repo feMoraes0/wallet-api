c2bd9663dc129b9576989449653c25dd2b016df0
feMoraes0 <fernandomoraes.lopes@gmail.com>
2019-10-20 17:02:48 -0300

    Updating connections with new transfer model

--- database/index.js
+++ database/index.js
@@ -4,13 +4,16 @@ const dbConfig = require("../config/database");
 const User = require('../app/models/User');
 const Color = require('../app/models/Color');
 const Card = require('../app/models/Card');
+const Transfer = require('../app/models/Transfer');
 
 const connection = new Sequelize(dbConfig);
 
 User.init(connection);
 Color.init(connection);
 Card.init(connection);
+Transfer.init(connection);
 
 Card.associate(connection.models);
+Transfer.associate(connection.models);
 
 module.exports = connection;
