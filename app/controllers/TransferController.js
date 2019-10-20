35d1eb1139a7bac387ed548fdae1e1fbee4a3c25
feMoraes0 <fernandomoraes.lopes@gmail.com>
2019-10-20 17:11:14 -0300

    Adding routes and basic transfer controller

---
+++ app/controllers/TransferController.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  async index(request, response, next) {},
+  async show(request, response, next) {},
+  async store(request, response, next) {},
+  async update(request, response, next) {},
+  async delete(request, response, next) {},
+}
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const UserController = require('../controllers/UserController');
 const CardController = require('../controllers/CardController');
+const TransferController = require('../controllers/TransferController');
 
 const routes = express.Router();
 
@@ -20,5 +21,13 @@ routes.get('/user/:user_id/card/:card_id', CardController.show);
 routes.post('/user/:user_id/card', CardController.store);
 routes.delete('/user/:user_id/card/:card_id', CardController.delete);
 routes.put('/user/:user_id/card/:card_id', CardController.update);
+
+/* TRANSFER */
+routes.get('/user/:user_id/card/:card_id/transfers', TransferController.index);
+
+routes.get('/user/:user_id/card/:card_id/transfer/:transfer_id', TransferController.show);
+routes.post('/user/:user_id/card/:card_id/transfer', TransferController.store);
+routes.delete('/user/:user_id/card/:card_id/transfer/:transfer_id', TransferController.delete);
+routes.put('/user/:user_id/card/:card_id/transfer/:transfer_id', TransferController.update);
 
 module.exports = routes;
